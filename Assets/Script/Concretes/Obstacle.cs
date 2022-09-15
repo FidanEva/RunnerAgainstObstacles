85b78449a0766a4d242f007fa323ed80ced99fbf
FidanEva <86987094+FidanEva@users.noreply.github.com>
2022-09-15 17:33:53 +0400

    Initial commit

---
+++ Assets/Script/Concretes/Obstacle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RunnerAgainstObstacle.Scripts
+{
+    public class Obstacle : MonoBehaviour
+    {
+
+        PlayerMovement playerMovement;
+
+        private void Start()
+        {
+            playerMovement = GameObject.FindObjectOfType<PlayerMovement>();
+        } 
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.name == "Player")
+            {
+                // Kill the player
+                playerMovement.Die();
+            }
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
